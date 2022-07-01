f440ad9040a595822ad4948568fcb28efbdc8c45
Filipe Zaidan <felipezaidan10@gmail.com>
2022-07-01 14:05:06 -0300

    feat: add cluster

--- index.js
+++ index.js
@@ -1,64 +1,94 @@
 const express = require('express');
-const app = express();
-const Stream = require("node-rtsp-stream");
 const cors = require('cors');
-
+const cluster = require('cluster');
+const { cpus } = require('os');
+const process = require('process');
+const Stream = require("node-rtsp-stream");
 const PORT = 3001;
 let streamVideo;
 
-app.use(cors({
-  origin: '*'
-}), express.json());
+const handleGeneratePort = () => {
+  const port = Math.floor(Math.random() * 6553);
 
-app.listen(PORT, (err) => {
-  if (err) throw err
-  console.log(`> Ready on http://localhost:${PORT}`)
-})
+  if (port.toString().length < 4) {
+    return port * 10
+  }
 
-const startStream = (url = null) => {
+  return port;
+}
+
+const streamOptions = {
+  name: "Bunny",
+  ffmpegOptions: {
+    // "-f": "mpegts",
+    // "-codec:v": "mpeg1video",
+    // "-b:v": "1000k",
+    "-stats": "",
+    "-r": 25,
+    // "-bf": 0,
+    // "-codec:a": "mp2",
+    // "-ar": 44100,
+    // "-ac": 1,
+    // "-b:a": "128k",
+    // "-vf": "fps=30"
+  },
+}
+
+const startStream = (url = "", port) => {
   return streamVideo = new Stream({
-    name: "Bunny",
-    streamUrl: url,
-    wsPort: 5050,
-    ffmpegOptions: {
-      "-f": "mpegts",
-      "-codec:v": "mpeg1video",
-      "-b:v": "1000k",
-      "-stats": "",
-      "-r": 25,
-      "-bf": 0,
-      "-codec:a": "mp2",
-      "-ar": 44100,
-      "-ac": 1,
-      "-b:a": "128k",
-      "-vf": "fps=30"
-    },
+    ...streamOptions,
+    ... { streamUrl: url, wsPort: port }
   });
 }
 
-app.post('/', (req, res) => {
-  try {
-    const { streamUrl } = req.body
-    startStream(streamUrl);
-    res.send("SUCCESS");
-  } catch (e) {
-    res.send(`ERROR: ${e}`);
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+
+  for (let i = 0; i < cpus().length; i++) {
+    cluster.fork();
   }
-})
 
-app.get('/stop', (req, res) => {
-  try {
-    streamVideo.stop();
-    res.send("SUCCESS");
-  } catch (e) {
-    res.send(`ERROR: ${e}`);
-  }
-})
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  const app = express();
+  app.use(cors({
+    origin: '*'
+  }), express.json());
 
-app.get('/index.html', (req, res) => {
-  res.sendFile(__dirname + '/html/index.html');
-})
+  app.post('/', (req, res) => {
+    try {
+      const { streamUrl } = req.body
+      const { options } = startStream(streamUrl, handleGeneratePort());
+      res.json({ port: options.wsPort });
+    } catch (e) {
+      res.send(`ERROR: ${e}`);
+    }
+  })
 
-app.get('/jsmpeg', (req, res) => {
-  res.sendFile(__dirname + '/html/jsmpeg.min.js');
-})
+  app.get('/stop', (req, res) => {
+    try {
+      streamVideo.stop();
+      res.send("SUCCESS");
+    } catch (e) {
+      res.send(`ERROR: ${e}`);
+    }
+  })
+
+  app.get('/index.html', (req, res) => {
+    res.sendFile(__dirname + '/html/index.html');
+  })
+
+  app.get('/jsmpeg', (req, res) => {
+    res.sendFile(__dirname + '/html/jsmpeg.min.js');
+  })
+
+  app.listen(PORT, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${PORT}`)
+  })
+
+  console.log(`Worker ${process.pid} started`);
+}
+
