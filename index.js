77571a81e18e73e76344344705664b456c2c033a
ifcassianasl <cassianasilveira64@gmail.com>
2022-07-08 09:34:42 -0300

    Create var with ws port

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const Stream = require("node-rtsp-stream");
-const cors = require('cors');
+const cors = require("cors");
 
 const PORT = 3001;
+const WS_PORT = 5050;
 let streamVideo;
 
-app.use(cors({
-  origin: '*'
-}), express.json());
+app.use(
+  cors({
+    origin: "*",
+  }),
+  express.json()
+);
 
 app.listen(PORT, (err) => {
-  if (err) throw err
-  console.log(`> Ready on http://localhost:${PORT}`)
-})
+  if (err) throw err;
+  console.log(`> Ready on http://localhost:${PORT}`);
+});
 
 const startStream = (url = null) => {
-  return streamVideo = new Stream({
+  return (streamVideo = new Stream({
     name: "Bunny",
     streamUrl: url,
-    wsPort: 5050,
+    wsPort: WS_PORT,
     ffmpegOptions: {
       "-f": "mpegts",
       "-codec:v": "mpeg1video",
@@ -31,34 +35,36 @@ const startStream = (url = null) => {
       "-ar": 44100,
       "-ac": 1,
       "-b:a": "128k",
-      "-vf": "fps=30"
+      "-vf": "fps=30",
+      "-q": 4,
     },
-  });
-}
+  }));
+};
 
-app.post('/', (req, res) => {
+app.post("/", (req, res) => {
+  streamVideo?.stop();
   try {
-    const { streamUrl } = req.body
+    const { streamUrl } = req.body;
     startStream(streamUrl);
     res.send("SUCCESS");
   } catch (e) {
     res.send(`ERROR: ${e}`);
   }
-})
+});
 
-app.get('/stop', (req, res) => {
+app.get("/stop", (req, res) => {
   try {
     streamVideo.stop();
     res.send("SUCCESS");
   } catch (e) {
     res.send(`ERROR: ${e}`);
   }
-})
+});
 
-app.get('/index.html', (req, res) => {
-  res.sendFile(__dirname + '/html/index.html');
-})
+app.get("/index.html", (req, res) => {
+  res.sendFile(__dirname + "/html/index.html");
+});
 
-app.get('/jsmpeg', (req, res) => {
-  res.sendFile(__dirname + '/html/jsmpeg.min.js');
-})
+app.get("/jsmpeg", (req, res) => {
+  res.sendFile(__dirname + "/html/jsmpeg.min.js");
+});
